36e783c23f23391686d0947555901d776ba63133
Shivam@911339 <shivamraj.tiwari787898@gmail.com>
2022-11-25 00:47:34 +0530

    backslash

---
+++ 13-Chapter1-cipherSchool.py
@@ -0,0 +1,11 @@
+# ********print these following lines***********
+#this is \\ double backslash
+# this is /\/\/\/\/\ mountain
+# he is    awesome(use escape sequence instead of manual sapces)
+# \" \n \t \'   (print this as an output)
+print("this is \\\\ double backslash")
+print("these are /\\/\\/\\/\\")
+
+print("he is \t awesome")
+
+print("\\\" \\n \\t \\\'")
