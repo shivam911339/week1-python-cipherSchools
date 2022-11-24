f889b68a9dde8b11daf23155bd900a0a5c3b8bdd
Shivam@911339 <shivamraj.tiwari787898@gmail.com>
2022-11-24 20:23:14 +0530

    escape2

---
+++ 12-escapeSequencesAsNormaltext-cipherschool.py
@@ -0,0 +1,12 @@
+#output : Line A \n LineB
+
+print("Line A \\n Line B")
+print("Line A \\t Line B")
+print("Line B\\\\\\")
+
+# outpout: \" \'
+print("\\\" \\\' ")
+
+# \' - '(a)
+# \\ - \(b)     #adding both
+# \\\' - \'
