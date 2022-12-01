4a8d2735501219b0405481c598215aa699f74d9b
Shivam Kumar Tiwari <114800211+shivam911339@users.noreply.github.com>
2022-12-01 19:03:56 +0530

    and  or

---
+++ 40-and_or-CipherSchools.py
@@ -0,0 +1,16 @@
+name="abc"
+age=19
+if name=="abc" and age==19:
+    print("Condition is True")
+else: 
+    print("condition is nt true")   
+
+
+# or operator
+
+name="shivam"
+age=19
+if name=="tiwari"or age==19:
+    print("condition is true")
+else:
+    print("condition is not true")    
