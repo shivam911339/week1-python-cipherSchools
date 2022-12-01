6a6823221a3186a4f5a4d9b949ad74009c7ba34a
Shivam Kumar Tiwari <114800211+shivam911339@users.noreply.github.com>
2022-12-01 18:53:51 +0530

    stringFormating

---
+++ 24-stringformating-CipherSchool.py
@@ -0,0 +1,10 @@
+name="shivam"
+age=19
+print("hello "+name+" your age is " + str(19))
+# string formating
+# python2
+# python3
+# python3.6(best) 
+print("hello {} your age is {}".format(name,age))
+
+print(f"hello {name} your age is {age}")
