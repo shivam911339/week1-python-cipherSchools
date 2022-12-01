a9ce316aedffc09251dfa2b4cfb6eb7858211ecd
Shivam Kumar Tiwari <114800211+shivam911339@users.noreply.github.com>
2022-12-01 18:58:59 +0530

    FindandReplace

---
+++ 33-findandReplace-CipherSchool.py
@@ -0,0 +1,11 @@
+string="my is is name is shivam kumar tiwari"
+# print(string.replace(" ","_"))
+# print(string.replace("is","are",1))
+
+
+
+print(string.find("is")) 
+print(string.find("i"))
+print(string.find("m",1))
+print(string.find("is",6))
+
