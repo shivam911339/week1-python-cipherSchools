875db7f6a793474b63a3fe89d0e2dad3a738e9b5
Shivam Kumar Tiwari <114800211+shivam911339@users.noreply.github.com>
2022-12-01 19:06:39 +0530

    Exercise3

---
+++ 47-Exercise3-CipherSchool.py
@@ -0,0 +1,12 @@
+# exercise one of three 
+# sum of n natural number
+# ask a user for natural number(n)
+# print total from 1 to n
+
+number=int(input("Enter no. here"))
+total=0
+i=1
+while i<=number:
+    total=total+i
+    i=i+1
+print(total)    
